0e4fa277fa7d31c47845ce145cee3bc1ace2e03b
Oskar <oskardb@uia.no>
2025-03-31 23:42:38 +0200

    made function for loading and saving tasks

--- backend.mjs
+++ backend.mjs
@@ -1,2 +1,18 @@
 console.log("APP");
 
+const todoForm = document.getElementById("todoForm");
+const inputTask = document.getElementById("inputTask");
+const listOfTasks = document.getElementById("listOfTasks");
+
+let tasks = [];
+
+function loadTasks() {
+    const storedTasks = localStorage.getItem("tasks");
+    if (storedTasks) {
+        tasks = JSON.parse(storedTasks);
+    }
+}
+
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
